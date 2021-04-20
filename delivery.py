5993e744836557f22803148e610c688046f8a285
ultragorira <lorideluca1988@gmail.com>
2021-04-20 12:41:47 +0300

    Finalized delivery script

--- delivery.py
+++ delivery.py
@@ -8,7 +8,6 @@ input_csv_filename = r'C:\Scripts\Sentiment_Analysis\Moderator.csv'
 input_json_filename = r'C:\Scripts\Sentiment_Analysis\sample_data_final.json'
 entries_by_key = defaultdict(list)
 updated_list = []
-full_conv_results = []
 full_dict = {}
 
 with open(input_csv_filename, encoding='utf-8') as csv_file:
@@ -16,25 +15,28 @@ with open(input_csv_filename, encoding='utf-8') as csv_file:
         entries_by_key[row["key"]].append(row)
 
 for entry in entries_by_key:
-    if (entries_by_key[entry][-1]['output1'] != '' and entries_by_key[entry][-1]['key'].find('full')==-1):
+    if (entries_by_key[entry][-1]['output1'] != '' and entries_by_key[entry][-1]['status'] == 'done'):
         updated_list.append([entries_by_key[entry][-1]['key'], [entries_by_key[entry][-1]['input1'],entries_by_key[entry][-1]['input2'],entries_by_key[entry][-1]['output1']]])
-    else:
-        full_conv_results.append([entries_by_key[entry][-1]['key'], [entries_by_key[entry][-1]['input1'],entries_by_key[entry][-1]['input2'],entries_by_key[entry][-1]['output1']]])
 
-idx = 0
-idx_full = 0
 
 with open(input_json_filename, 'r', encoding='utf-8') as json_out_file:
     full_json = json.load(json_out_file)['sample_data']
-    for f in full_json:
-        selected_row = (f['post_number'])
-        if (f['post_number']=='1'):
-            full_json[idx].update({'sentiment': updated_list[idx][1], 'overall_sentiment': full_conv_results[idx_full][1]})
-            idx +=1
-            idx_full +=1
+    for f in range(len(full_json)):
+        temp_key = full_json[f]['post_id']+'_'+full_json[f]['post_number']
+        if (full_json[f]['post_number']=='1'):
+            for result in updated_list:
+                if (temp_key == result[0]):
+                    element_to_find = temp_key.split('_')[0]+'_'+'full'
+                    overall_sentiment = [element_to_find in sub_list for sub_list in updated_list]
+                    if (True in overall_sentiment):
+                        full_json[f].update({'sentiment': result[1], 'overall_sentiment': updated_list[overall_sentiment.index(True)][1]})
+                    else:
+                        full_json[f].update({'sentiment': result[1]})
         else: 
-            full_json[idx].update({'sentiment': updated_list[idx][1]})
-            idx +=1
+            for result in updated_list:
+                if (temp_key == result[0]):
+                    full_json[f].update({'sentiment': result[1]})
+
 
 full_dict['sample_data'] = full_json
 

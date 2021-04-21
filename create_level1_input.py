116e0b0e0fdc614bfee644eb435a42014d3b2a33
ultragorira <lorideluca1988@gmail.com>
2021-04-21 13:41:16 +0300

    Fixed sorting

--- create_level1_input.py
+++ create_level1_input.py
@@ -1,21 +1,29 @@
 #!python3
 import csv
 import json
+import ndjson
 import sys
 from itertools import groupby
 
 OUTPUT_FILENAME = "import.csv"
 MAIN_KEY = "sample_data"
 
-_script_name, input_filename = sys.argv
-
+#_script_name, input_filename = sys.argv
+input_filename = r'C:\Scripts\Sentiment_Analysis\LionBridge.json'
 print(f"Reading from '{input_filename}")
 
 with open(input_filename, encoding='utf-8') as f:
-    content = json.load(f)[MAIN_KEY]
+    #content = json.load(f)[MAIN_KEY]
+    content = ndjson.load(f)
 
-sorted_posts = sorted(content, key=lambda entry: (entry["post_id"], entry["post_number"]))
-grouped_posts = groupby(sorted_posts, key=lambda entry: entry["post_id"])
+#sorted_posts = sorted(content, key=lambda entry: (entry["post_id"], entry["post_number"]))
+#grouped_posts = groupby(sorted_posts, key=lambda entry: entry["post_id"])
+
+
+sorted_posts = sorted(content, key=lambda entry: (
+    int(entry["topic_id"]), int(entry["order_within_topic"])))
+grouped_posts = groupby(sorted_posts, key=lambda entry: entry["topic_id"])
+
 
 csv_rows = []
 
@@ -23,8 +31,9 @@ for post_id, grouped_post in grouped_posts:
     full_post = ""
 
     for post in grouped_post:
-        full_post += f"response_id: {post['response_id']}\n{post['text']}\n\n"
-        csv_rows.append([f"{post_id}_{post['post_number']}", post["text"]])
+        full_post += f"text_id: {post['text_id']}\n{post['raw_text']}\n\n"
+        csv_rows.append(
+            [f"{post_id}_{post['order_within_topic']}", post['raw_text']])
 
     csv_rows.append([f"{post_id}_full", full_post])
 
